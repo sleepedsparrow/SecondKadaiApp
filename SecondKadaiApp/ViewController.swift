d36ceab0a0ea050b3decbe702c86dafa4425ae63
momoka.kato <mezzo52841@gmail.com>
2020-11-20 17:27:06 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 加藤桃香 on 2020/11/20.
+//  Copyright © 2020 momoka.kato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
